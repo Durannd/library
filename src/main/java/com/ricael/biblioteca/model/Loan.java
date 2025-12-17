3be3b03a74360b85fef5e35d971933862163e995
Ricael <ricaelmenezes@gmail.com>
2025-12-17 20:49:38 -0300

    feat: Loan entity add

---
+++ src/main/java/com/ricael/biblioteca/model/Loan.java
@@ -0,0 +1,27 @@
+package com.ricael.biblioteca.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Loan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    private Long bookId;
+
+    private Instant loanDate;
+    private Instant returnDate;
+
+}
