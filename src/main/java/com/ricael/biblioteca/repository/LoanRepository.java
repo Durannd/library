50d1a6942f3de852e450cc698d435c2ba212025c
Ricael <ricaelmenezes@gmail.com>
2025-12-19 15:14:09 -0300

    feat: new methods on LoanRepository

--- src/main/java/com/ricael/biblioteca/repository/LoanRepository.java
+++ src/main/java/com/ricael/biblioteca/repository/LoanRepository.java
@@ -4,6 +4,13 @@ import com.ricael.biblioteca.model.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface LoanRepository extends JpaRepository<Loan, Long> {
+
+    Optional<List<Loan>> findByUserId(Long id);
+
+    List<Loan> findByBookId(Long id);
 
 }
