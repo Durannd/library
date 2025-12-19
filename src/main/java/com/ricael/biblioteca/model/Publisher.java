b3193a3411dbadc3ec3a2cfc7e8baa261d456642
Ricael <ricaelmenezes@gmail.com>
2025-12-19 14:22:15 -0300

    refact: refact publisher class

--- src/main/java/com/ricael/biblioteca/model/Publisher.java
+++ src/main/java/com/ricael/biblioteca/model/Publisher.java
@@ -1,12 +1,16 @@
 package com.ricael.biblioteca.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "tb_publishers")
 public class Publisher {
 
